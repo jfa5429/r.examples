c96b40b2c7668dfcad1f6f76484cff644c19fcea
jfa5429 <47832283+jfa5429@users.noreply.github.com>
2025-01-15 18:42:44 -0600

    Add files via upload

---
+++ VaR.Example.R
@@ -0,0 +1,103 @@
+
+#INITIAL PRICES
+prices.vec <- c(80.0400, 438.9000, 2717.8000)
+#MEAN RETURNS
+mean.vec <- c(.01, .01, .01)
+#VOLATILITIES
+sigma.vec <- c(0.019258352, 0.013240624, 0.009363993)
+
+#CORRELATION MATRIX
+cor.mat = matrix(
+  c(1, 0.22, .18, .22, 1, 0.42, 0.18, 0.42, 1), 
+  nrow = 3,   
+  ncol = 3,         
+  byrow = TRUE          
+)
+
+rownames(cor.mat) = c("WTI", "COPPER", "GOLD")
+colnames(cor.mat) = c("WTI", "COPPER", "GOLD")
+
+
+#Multiple correlated prices simulation
+CorrelatedGBM <- function(N, S0, mu, sigma, cor.mat) {
+  
+  mu <- as.matrix(mu)  #average daily asset price drift
+  sigma <- as.matrix(sigma)  #vol of daily log returns
+  GBMs <- matrix(nrow = N, ncol = nrow(mu))  #row = # of days in path, ncol = # of correlated assets in simulation
+  Wt <- matrix(rnorm(N * nrow(mu), 0, 1), ncol = nrow(mu))  #matrix of individual assets' daily RANDOM volatilities; simulated from Normal(0,1)
+  Wt <- apply(Wt, 2, cumsum)   #apply cumulative daily sum of volatilities; each day's price contains the cumulative volatility up to that point
+  chol.mat <- chol(cor.mat) # upper triangular cholesky decomposition of correlation matrix; use upper triangular matrix because observations are in rows; When the observations are in columns, multiply from the left with the lower triangular matrix. When the observations are in rows, multiply from the right with the upper triangular matrix
+  Wt <- Wt %*% chol.mat   #matrix multiplication of matrix of cumulative sum of random volatility with cholesky decomposition of correlation matrix; Geometrically, the Cholesky matrix transforms uncorrelated variables into variables whose variances and covariances are given by Σ
+  for (i in 1:nrow(mu)) {
+    GBMs[,i] <- GBM(N, sigma[i], mu[i] , S0[i], Wt[,i])
+  }
+  return(GBMs)
+}
+
+#correlated asset paths and graph
+cor.prices.sim <- CorrelatedGBM(N=5, prices.vec, mean.vec, sigma.vec, cor.mat)  #run function, save price simulations
+class(cor.prices.sim)
+cor.prices.sim <- data.frame(cor.prices.sim)
+tail(cor.prices.sim)
+colnames(cor.prices.sim) <- c("WTI", "COPPER", "GOLD")
+cor.prices.sim$day <- 1:nrow(cor.prices.sim)
+library(reshape2)
+cor.prices.sim.melt <- dplyr::select(cor.prices.sim, day, "WTI", "COPPER", "GOLD") %>% melt(id.vars = "day")
+(cor.prices.sim.gr <- ggplot(data = na.omit(cor.prices.sim.melt), aes(x = day, y = value, color = variable)) + geom_line() + scale_y_continuous(labels = scales::dollar) + ylab("") + xlab("") 
+  + labs(title = "Assets Simulation\nhistorical correlations") + theme(legend.title = element_blank(), legend.position = c(.7,.8), legend.direction = "horizontal", legend.text = element_text(size = 12)) 
+  + theme(text = element_text(size = 12))) 
+
+#Multiple correlated prices simulation - final price only
+CorrelatedGBM.last <- function(N, S0, mu, sigma, cor.mat) {
+  
+  mu <- as.matrix(mu)  #average daily asset price drift
+  sigma <- as.matrix(sigma)  #vol of daily log returns
+  GBMs <- matrix(nrow = N, ncol = nrow(mu))  #row = # of days in path, ncol = # of correlated assets in simulation
+  Wt <- matrix(rnorm(N * nrow(mu), 0, 1), ncol = nrow(mu))  #matrix of individual assets' daily volatilities
+  Wt <- apply(Wt, 2, cumsum)   #apply cumulative daily sum of volatilities; each day's price contains the cumulative volatility up to that point
+  chol.mat <- chol(cor.mat) #upper triangular cholesky decomposition of correlation matrix
+  Wt <- Wt %*% chol.mat   #matrix multiplication of matrix of cumulative sum of random volatility with cholesky decomposition of correlation matrix; Geometrically, the Cholesky matrix transforms uncorrelated variables into variables whose variances and covariances are given by Σ
+  for (i in 1:nrow(mu)) {
+    GBMs[,i] <- GBM(N, sigma[i], mu[i] , S0[i], Wt[,i])
+  }
+  return(tail(GBMs,1))
+}
+
+
+#Multiple simulations of LAST PRICE function
+#store each row of LAST PRICES in matrix
+N.5 <- 5
+rrs.fwd.last.price.mc.5 <- do.call(rbind, lapply(1:10000, function(x) CorrelatedGBM.last(N=N.5, prices.vec, mean.vec, sigma.vec, cor.mat)))
+colnames(rrs.fwd.last.price.mc.5) <- c("WTI", "COPPER", "GOLD")
+#convert matrix to data.frame
+rrs.fwd.last.price.mc.df.5 <- data.frame(rrs.fwd.last.price.mc.5)
+rrs.fwd.last.price.mc.mtm.5 <- data.frame(rrs.fwd.last.price.mc.5)
+quantile(rrs.fwd.last.price.mc.5[,1], c(.05, .01)) #95% and 99% quantiles: WTI
+quantile(rrs.fwd.last.price.mc.5[,2], c(.05, .01)) #95% and 99% quantiles: COPPER
+quantile(rrs.fwd.last.price.mc.5[,3], c(.05, .01)) #95% and 99% quantiles: GOLD
+
+
+strikes <- c(75, 410, 2675)
+size <- c(1000, 100, 2)
+
+rrs.fwd.last.price.mc.mtm.strikes.5 <- sweep(rrs.fwd.last.price.mc.mtm.5, 2, strikes, '-')  #negative because sweep subtracts strikes from prices
+rrs.fwd.last.price.mc.mtm.strikes.total.5 <- sweep(rrs.fwd.last.price.mc.mtm.strikes.5, 2, size, '*')
+rrs.fwd.last.price.mc.mtm.strikes.5$mtm.total <- rowMeans(rrs.fwd.last.price.mc.mtm.strikes.5)
+quantile(rrs.fwd.last.price.mc.mtm.strikes.5[,1], c(.05, .01))
+rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total <- rowSums(rrs.fwd.last.price.mc.mtm.strikes.total.5)
+tail(rrs.fwd.last.price.mc.mtm.strikes.5)
+tail(rrs.fwd.last.price.mc.mtm.strikes.total.5)
+
+
+#5 DAY VAR GRAPHs
+#MtM historgram with VaR calculations
+quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, c(.05, .01))
+
+
+(var.mtm.total.hist.5 <- ggplot(rrs.fwd.last.price.mc.mtm.strikes.total.5, aes(mtm.total)) + geom_histogram(fill="blue", bins = 100) + scale_x_continuous(labels = scales::dollar, breaks = seq(from=-50000, to = 50000, by=5000)) 
+  + xlab("MtM value") + ylab("") + labs(title = "Portfolio Value at Risk, 5 day") 
+  + geom_vline(xintercept = quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .05), color = "black") 
+  + annotate(geom = "text", label = paste("95% VaR =", scales::dollar(quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .05))), x=quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .05), y=400, vjust=1, hjust=-.05) 
+  + geom_vline(xintercept = quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .01), color = "red") 
+  + annotate(geom = "text", color = "red", label = paste("99% VaR =", scales::dollar(quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .01))), x=quantile(rrs.fwd.last.price.mc.mtm.strikes.total.5$mtm.total, .01), y=300, vjust=1, hjust=1))
+
